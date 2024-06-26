cad77e27d0af781b6615528c988e12319b8201df
Adem Han <adem.han@outlook.com>
2024-06-26 10:48:56 +0300

    Ders Sonu 1.1

--- Day1/Degiskenler.playground/Contents.swift
+++ Day1/Degiskenler.playground/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var ad = "Adem"
+var soyad = "Han"
+var yas = 23
+var boy = 1.84
+var basHarf = "A"
+var devamDurumu = true
+
+print(ad)
+print(soyad)
+print(yas)
+print(boy)
+print(basHarf)
+print(devamDurumu)
+
+var urun_id : Int = 3416
+var urun_adi : String = "Macbook Pro"
+var urun_adet : Int = 100
+var urun_fiyat : Int = 34999
+var urun_tedarikci : String = "Apple"
+
+print("Ürün id : \(urun_id)")
+print("Ürün adı : \(urun_adi)")
+print("Ürün adet : \(urun_adet)")
+print("Ürün fiyat : \(urun_fiyat)")
+print("Ürün tedarikçi : \(urun_tedarikci)")
