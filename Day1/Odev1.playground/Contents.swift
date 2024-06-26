1e9d1c3969f1439fc9f0377b9228aad2dd7a4786
Adem Han <adem.han@outlook.com>
2024-06-26 12:22:04 +0300

    Odev1 Sonu

---
+++ Day1/Odev1.playground/Contents.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+var ilce : String = "Osmangazi"
+var kita : String = "Asya"
+var faks : String = "+1-212-9876543"
+var postaKodu : Int = 16245
+var instaAdresi : String = "ademhannnn"
+var calistigiBolum : String = "yazılım"
+var urunMiktarı : Int = 16
+var musteriSoyadi : String = "Han"
+var odemeMiktari : Int = 182609
+var dogumTarihi : String = "22.11.2000"
+var borc : Int = 92800
+var medeniHal : String = "bekar"
+var videoYorumu : String = "çok güzel"
+var odemeSaati : String = "12:51"
+var eftMiktari : Int = 34500
+var satilanMiktar : Int = 280
+var telefonModeli : String = "7 Plus"
+var dergiAdi : String = "KAFKAOKUR"
+var basımTarihi : String = "Haziran 2024"
+var zamMiktari : String = "%18"
+var daireSayisi : Int = 15
+var enlem : String = "1735802.183472.1332"
+var boylam : String = "183223.438237"
+var yemekAdi : String = "Pizza"
+var urunFiyati : Int = 135
+var sirket : String = "Trendyol"
+var videoAdi : String = "Duman - Kufi"
+var müzikSuresi : Double = 3.54
+var mekanPuani : Double = 4.3
+var dosyaAdi : String = "odev1"
+var resimFormati : String = ".img"
+var renk : String = "kırmızı"
+var renkKodu : Int = 14528472
+var bilgisayarModeli : String = "Macbook Air M2"
+var ekranBoyutu : Double = 12.4
+var kullanimSüresi : String = "3 ay"
+var basinc : String = "3 atm"
+var etkinlikGunu : String = "perşembe"
+var odemeGunu : String = "pazar"
+var yolculukTarihi : String = "24 haziran çarşamba"
+var mahalleAdi : String = "Sakarya"
+var otobüsHattı : String = "35B"
+var kullanilanDakika : Int = 13536
+var kargoTakipNo : Int = 17362178
+var kuponSüresi : Int = 18
+var kuponKodu : String = "SAHANECUMA47"
+var faturaTarihi : String = "22.04.2024"
+
+
+print("ilce : \(ilce) ")
+print("fatura tarihi : \(faturaTarihi)")
+print(kuponKodu)
