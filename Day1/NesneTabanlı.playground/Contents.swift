93ff048feeffbd8cd1459cdedea78c02940d64de
Adem Han <adem.han@outlook.com>
2024-08-03 19:32:26 +0300

    Enumeration ve Composition tamamlandı

--- Day1/NesneTabanlı.playground/Contents.swift
+++ Day1/NesneTabanlı.playground/Contents.swift
@@ -131,18 +131,92 @@ f.carpma(sayi1: 5, sayi2: 7)
 
 //statik değişkenler ve metodlar
 class ASinifi {
-    var x = 10
+    static var x = 10
     
-    func metod() {
+    static func metod() {
         print("Metod Çalıştı")
     }
 }
 
 let a = ASinifi()
 
-print(a.x)
+/* print(a.x)
 
 print(ASinifi().x)  //Sanal nesne - virtual object - isimsiz nesne
 
-ASinifi().metod()
+ASinifi().metod() */
 
+print(ASinifi.x)
+
+ASinifi.metod()
+
+
+//Enumeration
+
+enum KonserveBoyut {
+    case kucuk
+    case orta
+    case buyuk
+}
+
+func ucretHesapla(boyut: KonserveBoyut, adet: Int) {
+    switch boyut {
+        case .kucuk: print("Fiyat: \(adet * 13) tl")
+        case .orta: print("Fiyat: \(adet * 24) tl")
+        case .buyuk: print("Fiyat: \(adet * 45) tl")
+    }
+}
+
+ucretHesapla(boyut: .orta, adet: 2)
+
+//Composition
+
+class Kategoriler {
+    var kategori_id:Int?
+    var kategori_ad:String?
+    
+    init(kategori_id: Int, kategori_ad: String) {
+        self.kategori_id = kategori_id
+        self.kategori_ad = kategori_ad
+    }
+}
+
+class Yonetmenler {
+    var yonetmen_id:Int?
+    var yonetmen_ad:String?
+    
+    init(yonetmen_id: Int, yonetmen_ad: String) {
+        self.yonetmen_id = yonetmen_id
+        self.yonetmen_ad = yonetmen_ad
+    }
+}
+
+class Filmler {
+    var film_id:Int?
+    var film_ad:String?
+    var film_yil:Int?
+    var kategori:Kategoriler?
+    var yonetmen:Yonetmenler?
+    
+    init(film_id: Int, film_ad: String, film_yil: Int, kategori: Kategoriler, yonetmen: Yonetmenler) {
+        self.film_id = film_id
+        self.film_ad = film_ad
+        self.film_yil = film_yil
+        self.kategori = kategori
+        self.yonetmen = yonetmen
+    }
+}
+
+let k1 = Kategoriler(kategori_id: 1, kategori_ad: "Dram")
+let k2 = Kategoriler(kategori_id: 2, kategori_ad: "Komedi")
+
+let y1 = Yonetmenler(yonetmen_id: 1, yonetmen_ad: "Tarantino")
+let y2 = Yonetmenler(yonetmen_id: 2, yonetmen_ad: "Nolan")
+
+let f1 = Filmler(film_id: 1, film_ad: "Django", film_yil: 2013, kategori: k1, yonetmen: y1)
+
+print("Film id : \(f1.film_id!)")
+print("Film ad : \(f1.film_ad!)")
+print("Film yıl : \(f1.film_yil!)")
+print("Film kategori : \(f1.kategori!.kategori_ad!)")
+print("Film yönetmen : \(f1.yonetmen!.yonetmen_ad!)")
