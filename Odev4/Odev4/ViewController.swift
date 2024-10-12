ad6dfd1224580f9107e75f891f34991ab103a666
Adem Han <adem.han@outlook.com>
2024-10-12 19:25:33 +0300

    ödev 5 kişiselleştirilmiş

---
+++ Odev4/Odev4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Odev4
+//
+//  Created by Adem Han on 12.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
